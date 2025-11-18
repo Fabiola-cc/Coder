b6cc733394ae01d395ecada87de587cd682399e3
Fabiola Contreras <fcontrerascol@gmail.com>
2025-11-17 19:55:45 -0600

    fix: remove introduced bug

--- codificador/src/main/java/com/fmd/MIPSGenerator.java
+++ codificador/src/main/java/com/fmd/MIPSGenerator.java
@@ -645,7 +645,7 @@ public class MIPSGenerator {
 
         // CASO 5: Variable normal
         String srcReg = allocator.getReg(src);
-        String destReg = allocator.getRegAssign(dest, src);
+        String destReg = allocator.getReg(dest);
 
         // Evitar move redundante (move $t0, $t0)
         if (!destReg.equals(srcReg)) {
